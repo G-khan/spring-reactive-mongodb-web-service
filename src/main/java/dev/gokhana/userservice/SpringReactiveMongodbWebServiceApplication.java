0a884be832ef9c16883dd8b3847ffd540ba66111
Gökhan Ayrancıoğlu <gkhan.a.06@gmail.com>
2022-01-22 12:41:12 +0300

    First commit

---
+++ src/main/java/dev/gokhana/userservice/SpringReactiveMongodbWebServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.gokhana.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveMongodbWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactiveMongodbWebServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/gokhana/userservice/SpringReactiveMongodbWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gokhana.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveMongodbWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
